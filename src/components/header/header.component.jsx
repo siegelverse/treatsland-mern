b7d4d6bfa05ac79ef0bada30af99ac1939339f12
siegelverse <msiegel37@gmail.com>
2021-02-23 17:01:22 -0600

    added header componenet with links

---
+++ src/components/header/header.component.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './header.styles.scss';
+import { Link } from 'react-router-dom';
+import { ReactComponent as Logo } from '../../assets/treatlandmouth_new.svg'
+
+const Header = () => (
+    <div className='header'>
+        <Link className='logo-container' to='/'>
+            <Logo className='logo' />
+        </Link>
+        <div className='options'>
+            <Link to='/shop' className='option'>SHOP</Link>
+        </div>
+    </div>
+)
+
+export default Header;
