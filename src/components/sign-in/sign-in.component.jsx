c3a7aebe2ce37781847c60c38349aeba6b8e5273
siegelverse <msiegel37@gmail.com>
2021-03-01 14:13:30 -0600

    created sign up page

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import CustomButton from '../custom-button/custom-button.componenet';
+import CustomButton from '../custom-button/custom-button.component';
 import FormInput from '../form-input/form-input.component';
 import { signInWithGoogle } from '../../firebase/firebase.utils';
 import './sign-in.styles.scss';
