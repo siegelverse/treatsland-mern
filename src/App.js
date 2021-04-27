4b4692dc2e2aec3d39098447100365191a1bd7b4
siegelverse <msiegel37@gmail.com>
2021-04-26 21:25:07 -0500

    added checkout page && checkout component + cart selector

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import { connect } from 'react-redux';
 import { setCurrentUser } from './redux/user/user.actions';
 import { createStructuredSelector } from 'reselect';
 import { selectCurrentUser } from './redux/user/user.selectors';
+import CheckoutPage from './pages/checkout/checkout.component';
 
 class App extends React.Component {
 
@@ -46,6 +47,7 @@ class App extends React.Component {
           <Route exact path='/' component={HomePage} />
           <Route path='/shop' component={ShopPage} />
           <Route exact path='/signin' render={() => this.props.currentUser ? (<Redirect to='/' />) : (<SignInSignUpPage />)} />
+          <Route exact path='/checkout' component={CheckoutPage} />
         </Switch>
       </div>
     );
--- src/redux/cart/cart.selectors.js
+++ src/redux/cart/cart.selectors.js
@@ -6,6 +6,12 @@ export const selectCartItems = createSelector([selectCart], (cart) => cart.cartI
 
 export const selectCartHidden = createSelector([selectCart], cart => cart.hidden)
 
+export const selectCartTotal = createSelector(
+    [selectCartItems], 
+    cartItems => 
+    cartItems.reduce((totalQuantity, cartItem) => totalQuantity + cartItem.quantity * cartItem.price, 0)
+) 
+
 export const selectCartItemsCount = createSelector(
     [selectCartItems], 
     cartItems => 
