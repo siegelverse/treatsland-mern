ad18f6781898745f5a4751352b001281d7972fd9
siegelverse <msiegel37@gmail.com>
2021-05-17 14:52:20 -0500

    category currying for url params

--- src/pages/category/category.component.jsx
+++ src/pages/category/category.component.jsx
@@ -1,11 +1,18 @@
 import React from 'react';
 import { default as CollectionItem } from '../../components/collection-item/collection-item.component';
+import { connect } from 'react-redux';
+import { selectCollection } from '../../redux/shop/shop.selector';
 import './category.styles.scss';
 
-const CategoryPage = ({ match }) => (
+const CategoryPage = ({ collection }) => (
     <div className='category'>
         <h2>{ match }</h2>
     </div>
 );
+
+const mapStateToProps = (state, ownProps) => ({
+    collection: selectCollection(ownProps.match.params.collectionId)(state)
+
+})
 
 export default CategoryPage;
--- src/redux/shop/shop.selector.js
+++ src/redux/shop/shop.selector.js
@@ -1,8 +1,21 @@
 import { createSelector } from 'reselect'; 
 
+const COLLECTION_ID_MAP = {
+    puch: 1, 
+    honda: 2,
+    peugoet: 3,
+    parts: 4,
+    apparel: 5
+}
+
 const selectShop = state => state.shop;
 
 export const selectCollections = createSelector(
     [selectShop], 
     shop => shop.collections
+)
+
+export const selectCollection = createSelector(
+    [selectCollections],
+    collections => collections.find(collection => collection.id === COLLECTION_ID_MAP[collectionUrlParam])
 )
